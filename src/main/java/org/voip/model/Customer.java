b5cd5d59d3150ad1de8f9c85d179ecae1f0f61ad
rkaran <rk.karn32@gmail.com>
2014-11-25 15:29:22 -0600

    Customer model updated

--- src/main/java/org/voip/model/Customer.java
+++ src/main/java/org/voip/model/Customer.java
@@ -1,5 +1,55 @@
 package org.voip.model;
 
-public class Customer {
+import javax.persistence.Entity;
 
+@Entity
+public class Customer {
+	private int phoneNumber;
+	private Service service;
+	
+	//Address 
+	private String name;
+	private String city;
+	private String state;
+	private int zip;
+	
+	
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public Service getService() {
+		return service;
+	}
+	public void setService(Service service) {
+		this.service = service;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	
+	
 }
