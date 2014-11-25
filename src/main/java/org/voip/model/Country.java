8248c73e6a7c367f9c946f98fd53d05d0fe04116
pjayswal <free8ird@gmail.com>
2014-11-25 14:32:52 -0600

    Country added for testing

--- src/main/java/org/voip/model/Country.java
+++ src/main/java/org/voip/model/Country.java
@@ -1,7 +1,42 @@
 package org.voip.model;
 
-public class Country {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+
+@Entity
+public class Country {
+	@Id
+	private int code;
+	private String name;
+	private int peakTime;
+	private int offPeakTime;
+	
+	
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPeakTime() {
+		return peakTime;
+	}
+	public void setPeakTime(int peakTime) {
+		this.peakTime = peakTime;
+	}
+	public int getOffPeakTime() {
+		return offPeakTime;
+	}
+	public void setOffPeakTime(int offPeakTime) {
+		this.offPeakTime = offPeakTime;
+	}
 	
 
 }
