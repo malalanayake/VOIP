9b56878a75fe0ad3275e7e8eb38ec52a3a951b01
pjayswal <free8ird@gmail.com>
2014-11-26 17:52:43 -0600

    CallRateProcessing partially done

--- src/main/java/org/voip/service/CallRatesDataProcessor.java
+++ src/main/java/org/voip/service/CallRatesDataProcessor.java
@@ -1,6 +1,18 @@
 package org.voip.service;
 
 import java.io.FileInputStream;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.voip.dao.CallRateDAO;
+import org.voip.dao.CountryDAO;
+import org.voip.dao.CountryServiceDAO;
+import org.voip.model.Country;
 
 /**
  * Process the call rate data
@@ -8,12 +20,64 @@ import java.io.FileInputStream;
  * @author malalanayake
  *
  */
+@Service
 public class CallRatesDataProcessor implements ProcessData {
+
+	@Autowired
+	private CallRateDAO callRateDAO;
+	@Autowired
+	private CountryDAO countryDAO;
+	@Autowired
+	private CountryServiceDAO countryServiceDAO;
 
 	@Override
 	public boolean process(FileInputStream inputStream) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+		try {
 
+			HSSFWorkbook workbook = new HSSFWorkbook(inputStream);
+			for (int sheetNo = 0; sheetNo < workbook.getNumberOfSheets(); sheetNo++) {
+				HSSFSheet sheet = workbook.getSheetAt(sheetNo);
+				String sheetName= sheet.getSheetName();
+				String[] tokens=sheetName.split("_");
+				
+				Iterator<Row> rowIterator = sheet.iterator();
+				boolean firstRow = true;
+				while (rowIterator.hasNext()) {
+
+					Row row = rowIterator.next();
+					if (firstRow) {
+						firstRow = false;
+						continue;
+					}
+
+					// For each row, iterate through all the columns
+					Iterator<Cell> cellIterator = row.cellIterator();
+					Country country = new Country();
+					while (cellIterator.hasNext()) {
+
+						Cell cell = cellIterator.next();
+						// Check the cell type and format accordingly
+						switch (cell.getCellType()) {
+
+						case Cell.CELL_TYPE_STRING:
+							country.setName(cell.getStringCellValue());
+							break;
+						case Cell.CELL_TYPE_NUMERIC:
+							System.out.print(cell.getNumericCellValue() + "n");
+							country.setCode((int) cell.getNumericCellValue());
+							break;
+
+						}
+					}
+					countryDAO.save(country);
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
+	}
 }
--- src/main/java/org/voip/service/CallsDataProcessor.java
+++ src/main/java/org/voip/service/CallsDataProcessor.java
@@ -1,16 +1,8 @@
 package org.voip.service;
 
 import java.io.FileInputStream;
-import java.util.Iterator;
 
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.voip.dao.CountryDAO;
-import org.voip.dao.CustomerDAO;
-import org.voip.model.CallDetail;
-import org.voip.model.Country;
+import org.springframework.stereotype.Service;
 
 /**
  * Process calls data
@@ -18,51 +10,13 @@ import org.voip.model.Country;
  * @author malalanayake
  *
  */
+@Service
 public class CallsDataProcessor implements ProcessData {
-	
-	private CustomerDAO customerDAO;
-	
-	private CountryDAO countryDAO;
+
 	@Override
 	public boolean process(FileInputStream inputStream) {
-		try {
-
-			HSSFWorkbook workbook = new HSSFWorkbook(inputStream);
-			HSSFSheet sheet = workbook.getSheetAt(0);
-
-			// Iterate through each rows one by one
-			Iterator<Row> rowIterator = sheet.iterator();
-			while (rowIterator.hasNext()) {
-				Row row = rowIterator.next();
-				// For each row, iterate through all the columns
-				Iterator<Cell> cellIterator = row.cellIterator();
-				CallDetail call = new CallDetail();
-				while (cellIterator.hasNext()) {
-					
-					Cell cell = cellIterator.next();
-					
-					switch (cell.getCellType()) {
-
-					case Cell.CELL_TYPE_STRING:
-						//country.setName(cell.getStringCellValue());
-						break;
-					case Cell.CELL_TYPE_NUMERIC:
-						System.out.print(cell.getNumericCellValue() + "n");
-						//country.setCode((int)cell.getNumericCellValue());
-						break;
-
-					}
-				}
-				//countryDAO.save(country);
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-
-		return true;
-
+		return false;
 	}
 
+			
 }
