3e084dbf0b686e70c0a59ec51d3464c77ef4a334
malalanayake <dinuka.malalanayake@gmail.com>
2014-11-29 14:20:38 -0600

    Complete Monthly Billing report structure

---
+++ src/main/java/org/voip/service/CustomerService.java
@@ -0,0 +1,16 @@
+package org.voip.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.voip.dao.CustomerDAO;
+import org.voip.model.Customer;
+
+@Service
+public class CustomerService {
+	@Autowired
+	CustomerDAO customerDAO;
+	
+	public Customer getCustomerById(long phoneNumber){
+		return customerDAO.findOne(phoneNumber);
+	}
+}
--- src/main/java/org/voip/service/report/MonthlyBillReport.java
+++ src/main/java/org/voip/service/report/MonthlyBillReport.java
@@ -1,9 +1,19 @@
 package org.voip.service.report;
 
-import java.io.File;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
+import org.voip.dao.CustomerMonthlyReportDAO;
+import org.voip.model.Customer;
+import org.voip.model.report.CustomerMonthlyReport;
 
 /**
  * Monthly billing report generator
@@ -11,15 +21,41 @@ import org.springframework.web.servlet.ModelAndView;
  * @author malalanayake
  */
 public class MonthlyBillReport implements CustomReport {
+	private Date month;
+	private Customer customer;
 
-    @Override
-    public ModelAndView getReportTemplate() {
-        throw new UnsupportedOperationException("Not supported yet."); 
-    }
+	@Autowired
+	private CustomerMonthlyReportDAO customerMonthlyReportDAO;
+
+	public MonthlyBillReport(Date month, Customer customer) {
+		this.month = month;
+		this.customer = customer;
+	}
+
+	@Override
+	public ModelAndView getReportTemplate() {
+
+		Map<String, Object> parameterMap = new HashMap<String, Object>();
+		List<CustomerMonthlyReport> listOfRecords = customerMonthlyReportDAO
+				.getReportByMonthACustomer(month, customer.getPhoneNumber());
+		JRDataSource JRdataSource = new JRBeanCollectionDataSource(
+				listOfRecords);
+
+		parameterMap.put("datasource", JRdataSource);
+		parameterMap.put("header", "RATE SHEET");
+		parameterMap.put("month", this.month);
+		parameterMap.put("customer", this.customer.getName());
+		parameterMap.put("service", this.customer.getCountryService()
+				.getService().getName());
+
+		ModelAndView modelAndView = new ModelAndView("monthlyBillPdfReport",
+				parameterMap);
+		return modelAndView;
+	}
 
 	@Override
 	public void wireBeans(BeanFactory beanFactory) {
-
+		customerMonthlyReportDAO = beanFactory.getBean(CustomerMonthlyReportDAO.class);
 	}
 
 }
