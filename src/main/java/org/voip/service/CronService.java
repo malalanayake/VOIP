105876a8234a8d3fc78be711ecd61f0bfb527fb2
pjayswal <free8ird@gmail.com>
2014-12-02 13:04:55 -0600

    CronService added

---
+++ src/main/java/org/voip/service/CronService.java
@@ -0,0 +1,31 @@
+package org.voip.service;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.voip.dao.CustomerDAO;
+import org.voip.model.Customer;
+@Service
+public class CronService {
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	
+	@Scheduled(cron="0 0 24 L * ?")
+	public void sendMonthlyReportToCustomers(){
+		Iterable<Customer> customers = customerDAO.findAll();
+		Date date = new Date();
+		for(Customer customer: customers){
+			//PDF GENERATION MODULE
+			sendEmailToCustomer(customer);
+		}
+		
+	}
+	public void sendEmailToCustomer(Customer customer){
+		//Email Sending Module
+		System.out.println("Monthly Billing Report Sent to Customer:"+customer.getName());
+	}
+
+}
