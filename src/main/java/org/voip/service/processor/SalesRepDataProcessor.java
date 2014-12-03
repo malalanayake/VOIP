6d636b6f15e7ebf558e5c5d24f3fa8828179c312
malalanayake <dinuka.malalanayake@gmail.com>
2014-12-03 17:52:30 -0600

    Add Sales Rep Data processing Service

---
+++ src/main/java/org/voip/service/processor/SalesRepDataProcessor.java
@@ -0,0 +1,24 @@
+package org.voip.service.processor;
+
+import org.springframework.beans.factory.BeanFactory;
+
+/**
+ * Sales Rep Data Processing
+ * 
+ * @author malalanayake
+ *
+ */
+public class SalesRepDataProcessor implements DataProcessor {
+
+	@Override
+	public void wireBeans(BeanFactory beanFactory) {
+
+	}
+
+	@Override
+	public boolean process() {
+
+		return false;
+	}
+
+}
